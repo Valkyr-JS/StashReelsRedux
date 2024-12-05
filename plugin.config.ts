9fc6448441592192fac3850b0159877a473ca1b0
ValkyrJS <valkyr.photos@gmail.com>
2024-12-05 00:55:08 +0000

    Plugin directory setup

---
+++ plugin.config.ts
@@ -0,0 +1,38 @@
+import { resolve } from "path";
+import { defineConfig, normalizePath } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "plugin/main.tsx"),
+      name: "Stash Reels",
+      // the proper extensions will be added
+      fileName: "StashReelsRedux",
+      formats: ["umd"],
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["react"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: "React",
+        },
+        dir: "./plugin/dist",
+      },
+    },
+  },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: "./plugin/StashReelsRedux.yml",
+          dest: normalizePath(resolve(__dirname, "./plugin/dist")),
+        },
+      ],
+    }),
+  ],
+});
