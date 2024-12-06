399cb16830c300739974219c4a16f8446d99bd87
ValkyrJS <valkyr.photos@gmail.com>
2024-12-06 17:50:45 +0000

    fixed build path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/plugin/StashReels/assets/app/",
+  base: "/plugin/StashReelsRedux/assets/app/",
   build: {
     rollupOptions: {
       output: {
