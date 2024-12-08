b4ded3d1e5b43453ddaea2212c437522080507f7
ValkyrJS <valkyr.photos@gmail.com>
2024-12-08 16:51:20 +0000

    Scene studio logo

--- src/components/SceneInfoPanel/SceneInfoPanel.stories.tsx
+++ src/components/SceneInfoPanel/SceneInfoPanel.stories.tsx
@@ -1,45 +1,21 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import SceneInfoPanel from "./SceneInfoPanel";
 
-const defaultSceneData: Scene = {
-  title: "Old Man Take On 4 Buff Dudes",
-  created_at: "2024-12-08T03:16:40Z",
-  files: [],
-  galleries: [],
-  groups: [],
-  id: "1",
-  interactive: false,
-  movies: [],
-  o_history: [],
-  organized: true,
-  paths: {
-    __typename: undefined,
-    caption: undefined,
-    funscript: undefined,
-    interactive_heatmap: undefined,
-    preview: undefined,
-    screenshot: undefined,
-    sprite: undefined,
-    stream: undefined,
-    vtt: undefined,
-    webp: undefined,
+const defaultArgs = {
+  studio: {
+    image_path: import.meta.env.VITE_STASH_URI + "/studio/2/image?t=1733134208",
+    name: "Mia Melano",
   },
-  performers: [],
-  play_history: [],
-  sceneStreams: [],
-  scene_markers: [],
-  stash_ids: [],
-  tags: [],
-  updated_at: undefined,
-  urls: [],
+  title: "Birthday",
 };
 
 const meta: Meta<typeof SceneInfoPanel> = {
   title: "Components/Scene/InfoPanel",
   component: SceneInfoPanel,
   tags: ["autodocs"],
-  args: {
-    scene: defaultSceneData,
+  args: defaultArgs,
+  parameters: {
+    layout: "fullscreen",
   },
 };
 
@@ -47,3 +23,13 @@ export default meta;
 type Story = StoryObj<typeof SceneInfoPanel>;
 
 export const Default: Story = {};
+
+export const MinimalData: Story = {
+  args: {
+    studio: {
+      image_path: undefined,
+      name: "Mia Melano",
+    },
+    title: undefined,
+  },
+};
--- src/components/SceneInfoPanel/SceneInfoPanel.tsx
+++ src/components/SceneInfoPanel/SceneInfoPanel.tsx
@@ -2,15 +2,32 @@ import React from "react";
 import styles from "./SceneInfoPanel.module.scss";
 
 interface SceneInfoPanelProps {
-  scene: Scene;
+  studio: {
+    image_path: Studio["image_path"];
+    name: Studio["name"];
+  };
+  title: Scene["title"];
 }
 
 /** React component for the desktop-view scene info panel. */
 const SceneInfoPanel: React.FC<SceneInfoPanelProps> = (props) => {
+  /* ------------------------------------------- Header ------------------------------------------- */
+
+  // If there is an image for the studio, use it. Otherwise, create a text
+  // logo.
+  const studioLogo = props.studio.image_path ? (
+    <img src={props.studio.image_path} alt={props.studio.name} />
+  ) : (
+    <div className={styles["studio-name"]}>{props.studio.name}</div>
+  );
+
   return (
-    <div className={styles.SceneInfoPanel}>
-      <h1>{props.scene.title ?? "Untitled"}</h1>
-    </div>
+    <section className={styles.SceneInfoPanel}>
+      <div className="header">
+        <div className={styles["studio-logo"]}>{studioLogo}</div>
+        <h1>{props.title ?? "Untitled"}</h1>
+      </div>
+    </section>
   );
 };
 
