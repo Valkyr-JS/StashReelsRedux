b47978f865181ad440818f83764188e60c168244
ValkyrJS <valkyr.photos@gmail.com>
2024-12-10 13:53:03 +0000

    MiniButton click tests

--- src/components/SceneInfoPanel/SceneInfoPanel.stories.tsx
+++ src/components/SceneInfoPanel/SceneInfoPanel.stories.tsx
@@ -1,4 +1,6 @@
+import { expect } from "@storybook/jest";
 import type { Meta, StoryObj } from "@storybook/react";
+import { within, userEvent, waitFor } from "@storybook/testing-library";
 import SceneInfoPanel from "./SceneInfoPanel";
 import {
   MOCK_SCENE_O_RECORD,
@@ -31,6 +33,8 @@ type Story = StoryObj<typeof SceneInfoPanel>;
 
 export const Default: Story = {};
 
+/** The appearance of the scene info panel when only the minimum required data
+ * is passed. */
 export const MinimalData: Story = {
   args: {
     o_count: undefined,
@@ -40,5 +44,51 @@ export const MinimalData: Story = {
       name: "Mia Melano",
     },
     title: undefined,
+  },
+};
+
+/** Tests the play count button click event. */
+export const TestPlayCountClick: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const button: HTMLButtonElement = canvas.getByRole("button", {
+      name: `Play count ${MOCK_SCENE_PLAY_RECORD}`,
+    });
+    const initialValue = +button.value;
+
+    // Simulate click
+    await userEvent.click(button);
+
+    // Wait for the GQL mutation to resolve, then check that the button value has updated.
+    await waitFor(() => {
+      const updatedButton: HTMLButtonElement = canvas.getByRole("button", {
+        name: `Play count ${initialValue + 1}`,
+      });
+
+      expect(+updatedButton.value).toBe(initialValue + 1);
+    });
+  },
+};
+
+/** Tests the O count button click event. */
+export const TestOCountClick: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const button: HTMLButtonElement = canvas.getByRole("button", {
+      name: `O count ${MOCK_SCENE_O_RECORD}`,
+    });
+    const initialValue = +button.value;
+
+    // Simulate click
+    await userEvent.click(button);
+
+    // Wait for the GQL mutation to resolve, then check that the button value has updated.
+    await waitFor(() => {
+      const updatedButton: HTMLButtonElement = canvas.getByRole("button", {
+        name: `O count ${initialValue + 1}`,
+      });
+
+      expect(+updatedButton.value).toBe(initialValue + 1);
+    });
   },
 };
