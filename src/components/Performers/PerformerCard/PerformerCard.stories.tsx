2658ee75f8041a3fed926b6be45c22ca17acd191
ValkyrJS <valkyr.photos@gmail.com>
2025-01-16 18:39:53 +0000

    PerformerCard story data

--- src/components/Performers/PerformerCard/PerformerCard.stories.tsx
+++ src/components/Performers/PerformerCard/PerformerCard.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import PerformerCard from "./index";
+import performerData from "../../../../mocks/data/performers.json";
 
 const meta: Meta<typeof PerformerCard> = {
   title: "Components/Performers/PerformerCard",
@@ -10,8 +11,25 @@ const meta: Meta<typeof PerformerCard> = {
 export default meta;
 type Story = StoryObj<typeof PerformerCard>;
 
-export const Default: Story = {
+export const GisellePalmer: Story = {
   args: {
-    name: "Mia Smith",
+    ...performerData[1],
+    gender: performerData[1].gender as Performer["gender"],
   },
 };
+
+export const JohnnySins: Story = {
+  args: {
+    ...performerData[2],
+    gender: performerData[2].gender as Performer["gender"],
+  },
+};
+
+export const MiaMelano: Story = {
+  args: {
+    ...performerData[0],
+    gender: performerData[0].gender as Performer["gender"],
+  },
+};
+
+export const MinimalData: Story = {};
