7f953f7faab4dea929967aaad01a7abe604a6e86
ValkyrJS <valkyr.photos@gmail.com>
2025-01-17 17:09:56 +0000

    NextJS install

--- mocks/handlers/index.ts
+++
@@ -1,4 +0,0 @@
-/** https://mswjs.io/docs/network-behavior/graphql */
-import sceneMutationMocks from "./sceneMutationMocks";
-
-export const handlers = [...sceneMutationMocks];
--- src/helpers/index.ts
+++
@@ -1,2 +0,0 @@
-export * as dateHelpers from "./dates";
-export * as stashHelpers from "./stash";
