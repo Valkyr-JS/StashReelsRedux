815d6dd5dc087d30ee1645351441f3570c6e2922
ValkyrJS <valkyr.photos@gmail.com>
2025-01-18 23:58:39 +0000

    Removed global Apollo decorator and created it in a decorators file

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,5 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import type { Preview } from "@storybook/react";
 import { initialize, mswLoader } from "msw-storybook-addon";
-import React from "react";
 import { handlers } from "../mocks/handlers";
 
 /*
@@ -12,9 +10,6 @@ import { handlers } from "../mocks/handlers";
 initialize();
 
 const preview: Preview = {
-  decorators: [
-    (Story) => <ApolloProvider client={mockedClient}>{Story()}</ApolloProvider>,
-  ],
   loaders: [mswLoader], // Add the MSW loader to all stories
   parameters: {
     controls: {
@@ -34,21 +29,3 @@ const preview: Preview = {
 };
 
 export default preview;
-
-const mockedClient = new ApolloClient({
-  uri:
-    process.env.NODE_ENV === "development"
-      ? process.env["STASH_SERVER"] + "/playground"
-      : "/playground",
-  cache: new InMemoryCache(),
-  defaultOptions: {
-    watchQuery: {
-      fetchPolicy: "no-cache",
-      errorPolicy: "all",
-    },
-    query: {
-      fetchPolicy: "no-cache",
-      errorPolicy: "all",
-    },
-  },
-});
