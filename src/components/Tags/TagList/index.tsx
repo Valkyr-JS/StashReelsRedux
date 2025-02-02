ee41de17cb9043f1adffa7281ceba38c2253b48e
ValkyrJS <valkyr.photos@gmail.com>
2025-02-02 01:13:09 +0000

    TagList sorting

--- src/components/Tags/TagList/index.tsx
+++ src/components/Tags/TagList/index.tsx
@@ -8,10 +8,14 @@ interface TagListProps {
 }
 
 const TagList: React.FC<TagListProps> = (props) => {
+  // Sort tags by name by default, same as Stash. More sorting options may be
+  // added later.
+  const sortedTags = props.tags.sort((a, b) => a.name.localeCompare(b.name));
+
   return (
     <div className={styles.TagList}>
       <ul>
-        {props.tags.map((tProps) => (
+        {sortedTags.map((tProps) => (
           <li key={tProps.id}>
             <TagLink {...tProps} />
           </li>
